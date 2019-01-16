af1e1e318c8052b20a6f3c8d7c83b1e5845a0f8c
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-01-16 11:04:17 +0100

    Add demos for nodejs and browser.

---
+++ app.mjs
@@ -0,0 +1,3 @@
+import { healthInit } from './nms-map-handlers.mjs';
+
+console.log('hi from node', healthInit);
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import { healthInit } from './nms-map-handlers.mjs';
+
+console.log('index loaded');
+
+console.log('hi', healthInit)
